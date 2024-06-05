d6ad112a65d2436eceb99eb2ae701e3b0789939a
chrod <chrod.dev@gmail.com>
2024-06-05 15:37:15 -0500

    Add private screens layout 2

--- App.js
+++ App.js
@@ -2,23 +2,20 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Login from './src/screens/Login';
 import Signup from './src/screens/Signup';
+import PrivateScreens from './src/components/TabNavigator';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
 	return (
 		<NavigationContainer>
-			<Stack.Navigator initialRouteName='Login'>
-				<Stack.Screen
-					name='Login'
-					component={Login}
-					options={{ headerShown: false }}
-				/>
-				<Stack.Screen
-					name='Signup'
-					component={Signup}
-					options={{ headerShown: false }}
-				/>
+			<Stack.Navigator
+				initialRouteName='Login'
+				screenOptions={{ headerShown: false }}
+			>
+				<Stack.Screen name='Login' component={Login} />
+				<Stack.Screen name='Signup' component={Signup} />
+				<Stack.Screen name='PrivateScreens' component={PrivateScreens} />
 			</Stack.Navigator>
 		</NavigationContainer>
 	);
