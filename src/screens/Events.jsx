e2d0a1139d97e58ef73af6175d8f0fd0d2ad11cf
chrod <chrod.dev@gmail.com>
2024-06-08 17:53:58 -0500

    Add Eventos screen content

--- src/screens/Events.jsx
+++ src/screens/Events.jsx
@@ -1,16 +1,54 @@
-import { Text } from 'react-native';
+import { Alert, Image, StyleSheet, Text, View } from 'react-native';
 import PrivateLayout from '../components/layout/PrivateLayout';
+import { CalendarLgIcon } from '../utils/icons';
+import Button from '../components/ui/Button';
 
 const Events = () => {
+	const eventName = 'Sin eventos programados';
+	const guestsName = 'Agregue un invitado y se mostrará aquí';
+
 	return (
 		<PrivateLayout>
-			<Text
-				style={{ marginTop: 16, fontFamily: 'DMSansBold', color: '#FFFFFF' }}
-			>
-				Eventos
-			</Text>
+			<Text style={styles.screenName}>Eventos</Text>
+			<View style={styles.eventContainer}>
+				<Image source={CalendarLgIcon} />
+				<Text style={styles.eventName}>{eventName}</Text>
+				<Text style={styles.guestsName}>{guestsName}</Text>
+				<Button
+					label='Agregar Invitados'
+					onPress={() => Alert.alert('Agregando invitados!')}
+				/>
+			</View>
 		</PrivateLayout>
 	);
 };
+
+const styles = StyleSheet.create({
+	screenName: {
+		marginTop: 16,
+		fontFamily: 'DMSansBold',
+		fontSize: 18,
+	},
+	eventContainer: {
+		width: '80%',
+		minWidth: 250,
+		alignItems: 'center',
+		margin: 'auto',
+	},
+	eventName: {
+		marginTop: 16,
+		textAlign: 'center',
+		fontFamily: 'DMSansBold',
+		fontSize: 24,
+	},
+	guestsName: {
+		marginTop: 24,
+		marginBottom: 48,
+		textAlign: 'center',
+		fontFamily: 'DMSansBold',
+		fontSize: 18,
+		color: '#898F98',
+	},
+});
 
 export default Events;
